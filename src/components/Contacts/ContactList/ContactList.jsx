9042b6a60d26cde60e86e542054cc46145dbe5f2
AnuragM23 <anuragmahato006@gmail.com>
2024-10-16 14:25:34 +0530

    creating the card component for the contacts data

--- src/components/Contacts/ContactList/ContactList.jsx
+++ src/components/Contacts/ContactList/ContactList.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
+import { faEye, faPen, faPlusCircle, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 function ContactList() {
   return (
@@ -11,11 +11,11 @@ function ContactList() {
           <div className="grid">
             <div className="row">
               <div className="col">
-                <p className="h3">
+                <p className="h3 fw-bolder">
                   Contact List
                   <Link to={"/contact/add"} className="btn btn-primary mx-2">
                     {" "}
-                    <FontAwesomeIcon icon={faPlusCircle} className="me-2"/>
+                    <FontAwesomeIcon icon={faPlusCircle} className="me-2" />
                     Add New
                   </Link>
                 </p>
@@ -32,15 +32,63 @@ function ContactList() {
                 <form className="row">
                   <div className="col">
                     <div className="mb-2">
-                      <input type="text" className="form-control" placeholder="Search Names" />
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Search Names"
+                      />
                     </div>
                   </div>
                   <div className="col">
                     <div className="mb-2">
-                      <input type="submit" className="btn btn-outline-dark" value="Search"/>
+                      <input
+                        type="submit"
+                        className="btn btn-outline-dark"
+                        value="Search"
+                      />
                     </div>
                   </div>
                 </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="contact-list">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <div className="card">
+                <div className="card-body">
+                  <div className="row align-items-center d-flex justify-content-around">
+                    <div className="col-md-4">
+                      <img
+                        src="https://imgs.search.brave.com/q30mtGI6Uq8L1sU9H02hXDiETyRoSxEtuLtXNNmTvSw/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9wbHVz/cG5nLmNvbS9pbWct/cG5nL3VzZXItcG5n/LWljb24teW91bmct/dXNlci1pY29uLTI0/MDAucG5n"
+                        alt=""
+                        className="contact-img"
+                      />
+                    </div>
+                    <div className="col-md-7">
+                      <ul className="list-group">
+                        <li className="list-group-item list-group-item-action"> Name : <span className="fw-bold">Rajan</span></li> 
+                        <li className="list-group-item list-group-item-action"> Mobile : <span className="fw-bold">+91 9999999999</span></li> 
+                        <li className="list-group-item list-group-item-action"> Email : <span className="fw-bold">rajan@dummy.com</span></li> 
+                      </ul>
+                    </div>
+                    <div className="col-md-1 d-flex flex-column align-items-center gap-2">
+                      <Link to={`/contacts/view/:contactId`} className="btn btn-warning">
+                        <FontAwesomeIcon icon={faEye} />
+                      </Link>
+                      <Link to={`/contacts/view/:contactId`} className="btn btn-primary">
+                        <FontAwesomeIcon icon={faPen} />
+                      </Link>
+                      <button className="btn btn-danger">
+                        <FontAwesomeIcon icon={faTrash}/>
+                      </button>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
