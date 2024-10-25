b02cf0a28c6f65260cc9b57f364516b211a20d77
AnuragM23 <anuragmahato006@gmail.com>
2024-10-25 21:29:01 +0530

    added some changes

--- src/components/Contacts/ContactList/ContactList.jsx
+++ src/components/Contacts/ContactList/ContactList.jsx
@@ -76,7 +76,7 @@ function ContactList() {
 
   return (
     <>
-      <pre>{JSON.stringify(query)}</pre>
+      {/* <pre>{JSON.stringify(query)}</pre> */}
       <section className="contact-search p-3">
         <div className="container">
           <div className="grid">
